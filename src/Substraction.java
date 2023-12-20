ef68fbf444ce8535c75f4aad00dc034d6f76cb04
PunamDeokar <154241912+PunamDeokar@users.noreply.github.com>
2023-12-20 15:20:22 +0530

    Sub of three num Substraction.java

--- src/Substraction.java
+++ src/Substraction.java
@@ -4,7 +4,8 @@ public class Substraction {
 	public static void main(String[] args) {
 		int a =20;
 		int b=10;
-		int sub = a-b;
+		int c=5;
+		int sub = a-b-c;
 		System.out.println("Substraction is >> "+sub );
 	}
 }
